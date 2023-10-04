84ebdfcfc87b9eb8ef09134867ee31f4ca2716c6
TusharIbtekar <ibtekar8@gmail.com>
2023-10-04 22:44:49 +0600

    set up user model

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello via Bun!" });
+});
+
+app.post("/user", (req, res) => {
+  res.send({ message: "user created!" });
+});
+
+app.listen(3000, () => {
+  console.log("server running on port 3000");
+});
